bce53e1ecb32e0cdef8b7c3dbd0977cf0c8b3db0
GabrielVasconcelosBM <98931410+GabrielVasconcelosBM@users.noreply.github.com>
2025-10-03 12:12:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 function calcularRV() {
-    let prod = parseFloat(document.getElementById("prod").value) || 0;
 	let semana1 = parseInt(document.getElementById("semana1").value) || 0;
-    let clientes = parseInt(document.getElementById("clientes").value) || 0;
-    let visitas = parseInt(document.getElementById("visitas").value) || 0;
-    let m1_medio = parseFloat(document.getElementById("m1_medio").value) || 0;
-    let clientes_m1 = parseInt(document.getElementById("clientes_m1").value) || 0;
-    let m2_medio = parseFloat(document.getElementById("m2_medio").value) || 0;
-    let clientes_m2 = parseInt(document.getElementById("clientes_m2").value) || 0;
+	let semana2 = parseInt(document.getElementById("semana2").value) || 0;
+	let semana3 = parseInt(document.getElementById("semana3").value) || 0;
+	let semana4 = parseInt(document.getElementById("semana4").value) || 0;
+	let semana5 = parseInt(document.getElementById("semana5").value) || 0;
     
     let valorComissao = 0;
+	let valorPremiacao = 0;
 	let valorsemana1 = 0;
+	let valorsemana2 = 0;
+	let valorsemana3 = 0;
+	let valorsemana4 = 0;
+	let valorsemana5 = 0;
 
-    // Definição do valor comissão 
-    if (prod < 0.3) {
-        valorComissao = 0;
-    } else if (visitas < 18) {
-        valorComissao = clientes*20;
-    } else {
-        valorComissao = clientes*50;
-    }
 	
-	  // Definição do valor Semana1
+	// Definição do valor Semana1
 	if (semana1 < 2) {
 		valorsemana1 = 0;
 	} else if (semana1 >= 2 & semana1 < 3) {
@@ -33,48 +27,72 @@ function calcularRV() {
 		valorsemana1 = 700;
 	}
 
+	
+	// Definição do valor semana2
+	if (semana2 < 2) {
+		valorsemana2 = 0;
+	} else if (semana2 >= 2 & semana2 < 3) {
+		valorsemana2 = 100;
+	} else if (semana2 >= 3 & semana2 < 5) {
+		valorsemana2 = 450;
+	} else if (semana2 >= 5 & semana2 < 7) {
+		valorsemana2 = 550;
+	} else if (semana2 >= 7) {
+		valorsemana2 = 700;
+	}
 
 
-    // Definição do valor base para premiação
-    let valorBase = 0;
-    if (clientes <= 10) {
-        valorBase = 12;
-    } else if (clientes >= 11 && clientes <= 15) {
-        valorBase = 18;
-    } else if (clientes >= 16 && clientes <= 20) {
-        valorBase = 21;
-    } else {
-	valorBase = 30;
-    }
 
-    
-    // Definição do valor premiação
-    let valorPremiacao = 0;
-    if (m1_medio < 10000 && m2_medio < 10000) { 
-	valorPremiacao = 0;
-    } else if (m1_medio >= 10000 && m2_medio < 10000) {
-	valorPremiacao = clientes_m1*valorBase;
-    } else if (m1_medio < 10000 && m2_medio >= 10000) {
-	valorPremiacao = clientes_m2*valorBase;
-    } else {
-	valorPremiacao = clientes_m1*valorBase + clientes_m2*valorBase;    
-    }
-    
+	// Definição do valor semana3
+	if (semana3 < 2) {
+		valorsemana3 = 0;
+	} else if (semana3 >= 2 & semana3 < 3) {
+		valorsemana3 = 100;
+	} else if (semana3 >= 3 & semana3 < 5) {
+		valorsemana3 = 450;
+	} else if (semana3 >= 5 & semana3 < 7) {
+		valorsemana3 = 550;
+	} else if (semana3 >= 7) {
+		valorsemana3 = 700;
+	}
 
-    // Definição do valor premiação bônus
-    let bonus = 0;
-    if (prod >= 0.5 && m1_medio >= 15000) {
-        bonus = 500;
-    } else if (prod >= 0.3 && m1_medio >= 10000) {
-        bonus = 200; 
-    }
 
+		  // Definição do valor semana4
+	if (semana4 < 2) {
+		valorsemana4 = 0;
+	} else if (semana4 >= 2 & semana4 < 3) {
+		valorsemana4 = 100;
+	} else if (semana4 >= 3 & semana4 < 5) {
+		valorsemana4 = 450;
+	} else if (semana4 >= 5 & semana4 < 7) {
+		valorsemana4 = 550;
+	} else if (semana4 >= 7) {
+		valorsemana4 = 700;
+	}
+
+
+	// Definição do valor semana5
+	if (semana5 < 2) {
+		valorsemana5 = 0;
+	} else if (semana5 >= 2 & semana5 < 3) {
+		valorsemana5 = 100;
+	} else if (semana5 >= 3 & semana5 < 5) {
+		valorsemana5 = 450;
+	} else if (semana5 >= 5 & semana5 < 7) {
+		valorsemana5 = 550;
+	} else if (semana5 >= 7) {
+		valorsemana5 = 700;
+	}
+
+	// Definição do valor de Comissao
+	let valorComissao = valorsemana1 + valorsemana2 + valorsemana3 + valorsemana4 + valorsemana5;
+	
     // Salário fixo e ajuda de custo
     const salarioFixo = 1686.14;
     const ajudaCusto = 850.00;
 	
     // Soma final
-    let totalRV = salarioFixo + ajudaCusto + valorComissao + valorPremiacao + bonus;
+    let totalRV = salarioFixo + ajudaCusto + valorComissao + valorPremiacao;
 
     // Exibição dos resultados separados
     document.getElementById("resultado").innerHTML = `
@@ -82,12 +100,16 @@ function calcularRV() {
         <p><strong>Salário Fixo:</strong> <span>R$ ${salarioFixo.toFixed(2)}</span></p>
         <p><strong>Ajuda de Custo:</strong> <span>R$ ${ajudaCusto.toFixed(2)}</span></p>
 		<p><strong>Semana 1:</strong> <span>R$ ${valorsemana1.toFixed(2)}</span></p>
+		<p><strong>Semana 2:</strong> <span>R$ ${valorsemana2.toFixed(2)}</span></p>
+		<p><strong>Semana 3:</strong> <span>R$ ${valorsemana3.toFixed(2)}</span></p>
+		<p><strong>Semana 4:</strong> <span>R$ ${valorsemana4.toFixed(2)}</span></p>
+		<p><strong>Semana 5:</strong> <span>R$ ${valorsemana5.toFixed(2)}</span></p>
         <p><strong>Valor Comissão:</strong> <span>R$ ${valorComissao.toFixed(2)}</span></p>
         <p><strong>Valor Premiação:</strong> <span>R$ ${valorPremiacao.toFixed(2)}</span></p>
-        <p><strong>Valor Bônus:</strong> <span>R$ ${bonus.toFixed(2)}</span></p>
         <p><strong>Total Final:</strong> <span>R$ ${totalRV.toFixed(2)}</span></p>
     `;
 }
+
 
 
 
